9f63b09538bb75c559e8a01c57c2d167c9b47159
Cameron Reed Glowicki <crg@Camerons-Air.hitronhub.home>
2021-10-04 15:37:07 -0700

    refactor toggleReminder function to PUT status to db

--- src/App.vue
+++ src/App.vue
@@ -51,15 +51,30 @@ export default {
     async deleteTask(id) {
       if (confirm("Are you sure?")) {
         const res = await fetch(`api/tasks/${id}`, {
-          method: 'DELETE'
-        })
+          method: "DELETE",
+        });
 
-        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error deleting task");
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      });
+
+      const data = await res.json();
+
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
     toggleAddTask() {
